9ddf4861b7d13037c79c715d60b2276c17c031c1
qiongfa <3106226980@qq.com>
2023-02-07 19:55:56 +0800

    1

--- src/test/java/com/swy/community/CommunityApplicationTests.java
+++ src/test/java/com/swy/community/CommunityApplicationTests.java
@@ -2,8 +2,10 @@ package com.swy.community;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
 class CommunityApplicationTests {
 
     @Test
