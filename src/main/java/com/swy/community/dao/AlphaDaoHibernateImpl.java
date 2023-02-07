8f9fd2e0d0723059a8edd9755b0c39a46620aea6
qiongfa <3106226980@qq.com>
2023-02-07 17:17:46 +0800

    1

--- src/main/java/com/swy/community/dao/AlphaDaoHibernateImpl.java
+++ src/main/java/com/swy/community/dao/AlphaDaoHibernateImpl.java
@@ -3,6 +3,6 @@ package com.swy.community.dao;
 public class AlphaDaoHibernateImpl implements  AlphaDao {
     @Override
     public String select() {
-        return "qwe";
+        return "qwer";
     }
 }
