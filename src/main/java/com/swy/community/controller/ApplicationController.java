9c250b59148a2639131e94248072da7c2ad6fbac
qiongfa <3106226980@qq.com>
2023-02-08 21:06:51 +0800

    1

--- src/main/java/com/swy/community/controller/ApplicationController.java
+++ src/main/java/com/swy/community/controller/ApplicationController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/alpha")
@@ -24,5 +25,16 @@ public class ApplicationController {
     @ResponseBody
     public String getData() {
         return alphaService.find();
+    }
+
+
+    @RequestMapping("/teacher")
+    public ModelAndView teacher() {
+        System.out.printf("123");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "张三");
+        modelAndView.addObject("age", "18");
+        modelAndView.setViewName("/demo/view");
+        return modelAndView;
     }
 }
